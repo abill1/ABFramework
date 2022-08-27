ce5dba4ba289809e49738921c9cd203a5b395f72
Anthony Bill <36425922+abill1@users.noreply.github.com>
2022-08-27 18:15:02 -0500

    Added file for typedefs to redefine common data types.

---
+++ ABFramework/DataTypes/ABTypeDefs.h
@@ -0,0 +1,26 @@
+// © 2022 Anthony Bill. All rights reserved.
+
+//********************************************************************************
+// Redefinitions of common data types. 
+//********************************************************************************
+
+#include <stdint.h>
+
+#ifndef ABTYPE_DEF_H
+#define ABTYPE_DEF_H
+
+// ----- Signed integer / char types
+
+typedef int8_t Byte;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+// ----- Unsigned integer / char types
+
+typedef uint8_t uByte;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif // !ABTYPE_DEF_H
