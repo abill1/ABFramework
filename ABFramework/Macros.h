be1b771b5bb0bd60d0a52f12673d0d99f914dd2a
Anthony Bill <36425922+abill1@users.noreply.github.com>
2022-08-27 17:49:15 -0500

    Added macro for assertion operation to check for a nullptr.

--- ABFramework/Macros.h
+++ ABFramework/Macros.h
@@ -4,11 +4,14 @@
 // A header to include personal macro definitions. 
 //********************************************************************************
 
+#include <assert.h>
+
 #ifndef ABMACROS_H
 #define ABMACROS_H
 
 #define UNUSED_VAR(x) (void(x))
 #define IS_NULL(x) x == nullptr;
+#define CHECK_NULL(x) assert(x != nullptr);
 
 #endif // !ABMACROS_H
 
